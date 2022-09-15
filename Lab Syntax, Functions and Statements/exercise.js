10122e743a03e67bd177abcc6b6b61a266855458
Natali Angelova <natali.v.angelova@gmail.com>
2022-09-15 22:46:04 +0300

    Update

--- Lab Syntax, Functions and Statements/exercise.js
+++ Lab Syntax, Functions and Statements/exercise.js
@@ -38,5 +38,9 @@ function sameNumber(num) {
 }
 
 //04. Previous Day
-
+function previousDay(year, month, day) {
+  let date = new Date(year, month + 1, day);
+  date.setDate(date.getDate() - 1);
+  console.log(`${date.getFullYear()}-${date.getMonth() - 1}-${date.getDate()}`);
+}
 previousDay(2016, 10, 1);
