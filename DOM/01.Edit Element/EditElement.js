d0552dc8ede57384cf960e3d1e63aa286db5a430
Natali Angelova <natali.v.angelova@gmail.com>
2022-09-25 22:04:01 +0300

    DOM - Edit Element

--- DOM/01.Edit Element/EditElement.js
+++ DOM/01.Edit Element/EditElement.js
@@ -1,3 +1,6 @@
-function editElement() {
-    // TODO
+function editElement(ref, match, replacer) {
+  const content = ref.textContent;
+  const matcher = new RegExp(match, "g");
+  const edited = content.replace(matcher, replacer);
+  ref.textContent = edited;
 }
