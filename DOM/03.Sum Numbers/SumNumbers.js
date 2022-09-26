0e1d64e2857af79e7e179c88f912fd4a31fa60c5
Natali Angelova <natali.v.angelova@gmail.com>
2022-09-26 12:43:03 +0300

    DOM lab

--- DOM/03.Sum Numbers/SumNumbers.js
+++
@@ -1,5 +0,0 @@
-function calc() {
-  let firstnum = Number(document.getElementById("num1").value);
-  let secondnum = Number(document.getElementById("num2").value);
-  document.getElementById("sum").value = firstnum + secondnum;
-}
