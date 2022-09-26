48c52bb74bdfc821feea3c6fa9dc5b296cdcf0c3
Natali Angelova <natali.v.angelova@gmail.com>
2022-09-26 12:55:52 +0300

    DOM - exercise start resources

---
+++ DOM/exercise/02. Pascal or Camel Case/solution.js
@@ -0,0 +1,6 @@
+function solve() {
+  let text = document.getElementById("text").value;
+  let namingConvention = document.getElementById("naming-convention").value;
+  console.log(text);
+  console.log(namingConvention);
+}
