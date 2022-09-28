50b4bb3dec32fee04d9552bfda5eb4fe31b75792
Natali Angelova <natali.v.angelova@gmail.com>
2022-09-28 15:58:35 +0300

    DOM Exercise

--- DOM/exercise/08. Generate Report/report.js
+++ DOM/exercise/08. Generate Report/report.js
@@ -1,30 +1,52 @@
+// function generateReport() {
+//   debugger;
+//   let boxID = [];
+//   let allBoxes = Array.from(document.querySelectorAll("input"));
+
+//   allBoxes.forEach((el, index) => {
+//     if (el.checked) {
+//       boxID.push(index);
+//     }
+//   });
+
+//   let result = [];
+//   let rows = Array.from(document.querySelectorAll("tbody tr"));
+
+//   for (const row of rows) {
+//     let object = {};
+
+//     for (const index of boxID) {
+//       let name = allBoxes[index].name;
+//       let data = row.children[index].textContent;
+//       object[name] = data;
+//     }
+
+//     result.push(object);
+//   }
+
+//   let report = JSON.stringify(result);
+//   let output = document.getElementById("output");
+//   output.textContent = report;
+// }
+
 function generateReport() {
-  debugger;
-  let boxID = [];
-  let allBoxes = Array.from(document.querySelectorAll("input"));
+  let tableRow = document.querySelectorAll("tbody tr");
+  let tableHeadRow = document.querySelectorAll("thead tr th");
+  let res = [];
 
-  allBoxes.forEach((el, index) => {
-    if (el.checked) {
-      boxID.push(index);
+  for (let i = 0; i < tableRow.length; i++) {
+    let tableData = tableRow[i].cells;
+    let tempData = {};
+    for (let j = 0; j < tableData.length; j++) {
+      let infoHeader = tableHeadRow[j].childNodes;
+
+      if (infoHeader[1].checked) {
+        tempData[infoHeader[0].textContent.trim().toLocaleLowerCase()] =
+          tableData[j].textContent;
+      }
     }
-  });
-
-  let result = [];
-  let rows = Array.from(document.querySelectorAll("tbody tr"));
-
-  for (const row of rows) {
-    let object = {};
-
-    for (const index of boxID) {
-      let name = allBoxes[index].name;
-      let data = row.children[index].textContent;
-      object[name] = data;
-    }
-
-    result.push(object);
+    res.push(tempData);
   }
-
-  let report = JSON.stringify(result);
-  let output = document.getElementById("output");
-  output.textContent = report;
+  let jsonRes = JSON.stringify(res);
+  document.getElementById("output").textContent = jsonRes;
 }
