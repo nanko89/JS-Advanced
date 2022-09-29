73626928cfe8c4d00ab814d6a6a74d84c4bc9cf9
Natali Angelova <natali.v.angelova@gmail.com>
2022-09-29 17:46:42 +0300

    DOM Manipulations lab

--- DOM Manipulations/lab/06. Dynamic-Validation/app.js
+++ DOM Manipulations/lab/06. Dynamic-Validation/app.js
@@ -1,3 +1,14 @@
 function validate() {
-    console.log('TODO:...');
+  let email = document.getElementById("email");
+  let pattern = /[a-z]+@[a-z]+.[a-z]+/g;
+
+  email.addEventListener("change", function (e) {
+    let currentEmail = email.value;
+    if (pattern.test(currentEmail)) {
+      e.currentTarget.removeAttribute("class");
+      console.log(email.value);
+    } else {
+      e.currentTarget.classList.add("error");
+    }
+  });
 }
--- DOM Manipulations/lab/07. Shopping-Cart/solution.js
+++ DOM Manipulations/lab/07. Shopping-Cart/solution.js
@@ -1,3 +1,40 @@
 function solve() {
-   //TODO...
+  let products = [];
+  let totalPrice = 0;
+  let shoppingCart = document.getElementsByClassName("shopping-cart")[0];
+  let textArea = document.getElementsByTagName("textarea")[0];
+
+  let checkoutDone = false;
+
+  shoppingCart.addEventListener("click", function (e) {
+    let btn = e.target;
+    if (btn.nodeName !== "BUTTON") {
+      return;
+    }
+
+    if (checkoutDone) {
+      return;
+    }
+
+    if (Array.from(btn.classList).indexOf("add-product") >= 0) {
+      let productElement = btn.parentElement.parentElement;
+
+      let name =
+        productElement.querySelectorAll(".product-title")[0].textContent;
+      let money = productElement.querySelectorAll(".product-line-price")[0]
+        .textContent;
+
+      totalPrice += Number(money);
+      textArea.textContent += `Added ${name} for ${money} to the cart.\n`;
+
+      if (products.indexOf(name) < 0) {
+        products.push(name);
+      }
+    } else if (Array.from(btn.classList).indexOf("checkout") >= 0) {
+      checkoutDone = true;
+      textArea.textContent += `You bought ${products.join(
+        ", "
+      )} for ${totalPrice.toFixed(2)}.`;
+    }
+  });
 }
