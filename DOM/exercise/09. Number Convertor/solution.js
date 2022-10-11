26eb1c8d16617ade7843f6f0de3896b156dbced3
Natali Angelova <natali.v.angelova@gmail.com>
2022-10-11 16:33:44 +0300

    Update Dom Exercise

--- DOM/exercise/09. Number Convertor/solution.js
+++ DOM/exercise/09. Number Convertor/solution.js
@@ -1,4 +1,28 @@
 function solve() {
+  let selectMenu = document.getElementById("selectMenuTo");
 
-    //TODO...
+  let binaryOption = document.createElement("option");
+  binaryOption.textContent = "Binary";
+  binaryOption.value = "binary";
+
+  let hexadeicmalOption = document.createElement("option");
+  hexadeicmalOption.textContent = "Hexadecimal";
+  hexadeicmalOption.value = "hexadecimal";
+
+  selectMenu.appendChild(hexadeicmalOption);
+  selectMenu.appendChild(binaryOption);
+
+  let selectMap = {
+    binary: (num) => num.toString(2),
+    hexadecimal: (num) => num.toString(16).toUpperCase(),
+  };
+
+  let convertBtn = document.querySelector("#container button");
+
+  convertBtn.addEventListener("click", () => {
+    let inputData = document.getElementById("input");
+    let outputData = document.getElementById("result");
+
+    outputData.value = selectMap[selectMenu.value](+inputData.value);
+  });
 }
