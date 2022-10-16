9f80c530632b33c4176e34f5806e916b06031257
Natali Angelova <natali.v.angelova@gmail.com>
2022-10-16 21:59:43 +0300

    Update exam

--- Exams/Exam - 23 October 2021/03. Library/library.test.js
+++ Exams/Exam - 23 October 2021/03. Library/library.test.js
@@ -36,11 +36,54 @@ describe("Test Library", () => {
       );
     });
   });
+
   describe("Test findBook (booksArr, desiredBook) function", () => {
     it("should throw error if booksArr length is 0", () => {
       expect(() => {
         library.findBook([], "Book");
-      }).to.throw("");
+      }).to.throw("No books currently available");
+    });
+    it("should return message if desired book exist", () => {
+      expect(
+        library.findBook(["Troy", "Life Style", "Torronto"], "Troy")
+      ).to.be.equal("We found the book you want.");
+    });
+
+    it("should return messsage if desired book NOT exist", () => {
+      expect(
+        library.findBook(["Troy", "Life Style", "Torronto"], "The Book")
+      ).to.be.equal("The book you are looking for is not here!");
+    });
+  });
+
+  describe("Test arrangeTheBooks (countBooks) functions", () => {
+    it("should invalid input and throw an error", () => {
+      expect(() => {
+        library.arrangeTheBooks();
+      }).to.throw("Invalid input");
+      expect(() => {
+        library.arrangeTheBooks("5");
+      }).to.throw("Invalid input");
+      expect(() => {
+        library.arrangeTheBooks(-50);
+      }).to.throw("Invalid input");
+      expect(() => {
+        library.arrangeTheBooks(5.5);
+      }).to.throw("Invalid input");
+    });
+    it("should all the books are arranged on the shelves", () => {
+      expect(library.arrangeTheBooks(0)).to.be.equal(
+        "Great job, the books are arranged."
+      );
+      expect(library.arrangeTheBooks(40)).to.be.equal(
+        "Great job, the books are arranged."
+      );
+    });
+
+    it("should have no space in library", () => {
+      expect(library.arrangeTheBooks(45)).to.be.equal(
+        "Insufficient space, more shelves need to be purchased."
+      );
     });
   });
 });
